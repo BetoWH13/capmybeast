550261e2ab7995fc3000ad3a839e8acfbfc68bf5
BetoWH13 <awaizel@gmail.com>
2025-04-18 13:56:39 -0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+// service-worker.js
+const CACHE_NAME = 'capmybeast-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/beasts.json',
+  '/assets/beasts/beast-1.svg',
+  '/assets/beasts/beast-2.svg',
+  '/assets/beasts/beast-3.svg',
+  '/assets/beasts/beast-4.svg',
+  '/assets/beasts/beast-5.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+});
